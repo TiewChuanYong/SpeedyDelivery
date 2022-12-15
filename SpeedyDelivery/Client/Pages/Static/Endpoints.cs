e919b89ab32b361cb7f514820eb7001b17287fb6
TIEW CHUAN YONG <2102030J@student.tp.edu.sg>
2022-12-15 21:05:19 +0800

    Add project files.

---
+++ SpeedyDelivery/Client/Pages/Static/Endpoints.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpeedyDelivery.Client.Static
+{
+    public static class Endpoints
+    {
+        private static readonly string Prefix = "api";
+
+        public static readonly string MakesEndpoint = $"{Prefix}/restaurants";
+        public static readonly string ModelsEndpoint = $"{Prefix}/carts";
+        public static readonly string ColoursEndpoint = $"{Prefix}/foods";
+    }
+}
